14eeb843e411485c811c74242ad1dab1e85a3d77
Darrell-Conklin <dmconklin84@gmail.com>
2020-01-21 19:52:25 -0600

    Finished stretch goals

--- index.js
+++ index.js
@@ -84,6 +84,17 @@ Car.prototype.fill = function(gallons) {
   this.tank += gallons;
 }
 
+Car.prototype.drive = function(distance) {
+  if ((distance / this.milesPerGallon) > this.tank) {
+    this.odometer += this.tank * this.milesPerGallon;
+    this.tank = 0;
+    return `I ran out of gas at ${this.odometer} miles!`;
+  } else {
+    this.odometer += distance;
+    this.tank -= (distance / this.milesPerGallon);
+  }
+}
+
 /*
   TASK 3
     - Write a Baby constructor subclassing Person.
